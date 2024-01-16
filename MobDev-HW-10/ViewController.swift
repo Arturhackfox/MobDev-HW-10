2e6879b1414d2a43426c446b61400c8be3d6e7cf
Arthur Sh <arthur.shemshur@gmail.com>
2024-01-16 08:58:26 +0200

    Initial Commit

---
+++ MobDev-HW-10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobDev-HW-10
+//
+//  Created by Arthur Sh on 16.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
